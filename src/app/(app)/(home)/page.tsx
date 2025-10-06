e615f9e1366c8720f9a6600d406059c582cd5b56
Yuchen Ma <mayuchen0611@outlook.com>
2025-10-06 02:51:20 -0400

    15: multi tenancy

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,21 +1,30 @@
-// import { getQueryClient, trpc } from "@/trpc/server";
+import type { SearchParams } from "nuqs/server";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
-// export default async function Home() {
-//   const queryClient = getQueryClient();
-//   const categories = await queryClient.fetchQuery(
-//     trpc.categories.getMany.queryOptions()
-//   );
-
-//   return <div>{JSON.stringify(categories, null, 2)}</div>;
-// }
-"use client";
-
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-export default function Home() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.auth.session.queryOptions());
-
-  return <div>{JSON.stringify(data?.user, null, 2)}</div>;
+interface Props {
+  searchParams: Promise<SearchParams>;
 }
+
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadProductFilters(searchParams);
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...filters,
+      limit: DEFAULT_LIMIT,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
--- src/modules/home/ui/components/search-filters/category-dropdown.tsx
+++ src/modules/home/ui/components/search-filters/category-dropdown.tsx
@@ -2,7 +2,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
-import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
 import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
@@ -20,7 +19,6 @@ export const CategoryDropdown = ({
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const { getDropdownPosition } = useDropdownPosition(dropdownRef);
 
   const onMouseEnter = () => {
     if (category.subcategories) {
@@ -29,8 +27,6 @@ export const CategoryDropdown = ({
   };
 
   const onMouseLeave = () => setIsOpen(false);
-
-  const dropdownPosition = getDropdownPosition();
 
   // potentially improve mobile
   // const toggleDropdown = () => {
@@ -70,11 +66,7 @@ export const CategoryDropdown = ({
           />
         )}
       </div>
-      <SubcategoryMenu
-        category={category}
-        isOpen={isOpen}
-        position={dropdownPosition}
-      />
+      <SubcategoryMenu category={category} isOpen={isOpen} />
     </div>
   );
 };
