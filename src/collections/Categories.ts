d8630fb0ba2b15dca7189f00e89005643194da20
Yuchen Ma <mayuchen0611@outlook.com>
2025-10-09 12:24:18 -0400

    24: access control

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,7 +1,14 @@
+import { isSuperAdmin } from "@/lib/access";
 import type { CollectionConfig } from "payload";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
+  access: {
+    read: () => true,
+    create: ({ req }) => isSuperAdmin(req.user),
+    update: ({ req }) => isSuperAdmin(req.user),
+    delete: ({ req }) => isSuperAdmin(req.user),
+  },
   admin: {
     useAsTitle: "name",
   },
